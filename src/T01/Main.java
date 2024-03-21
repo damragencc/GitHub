207ab9b1469f5d00846c20de89f85f53cb3f36a7
damragencc <damragenc@gmail.com>
2024-03-21 17:02:01 +0300

    v4

--- src/T01/Main.java
+++ src/T01/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("main v1");
         System.out.println("main v2");
         System.out.println("main v3git switch ");
+        System.out.println("main v4");
     }
 }
